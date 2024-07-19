875d6fea7b1cfb9ca4504c36c12261c5f5f53f49
lucas5227 <klucas5227@gmail.com>
2024-07-19 17:39:26 +0900

    불필요한것 정리 HOME: safeAera  scrollView expo-image-picker ...

--- nabibox-front/app/(tabs)/_layout.tsx
+++ nabibox-front/app/(tabs)/_layout.tsx
@@ -1,17 +1,16 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
+// import React from 'react';
 
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+// import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
       }}>
       <Tabs.Screen
